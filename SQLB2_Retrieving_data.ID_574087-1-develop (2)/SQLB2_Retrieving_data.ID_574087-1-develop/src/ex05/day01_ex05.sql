3032ce43873e5420ad4e709009cb6a88256fa40b
khamrokulovaraikhona-07 <khamrokulovaraikhona07@gmail.com>
2026-04-27 17:44:48 +0500

    Delete SQLB2_Retrieving_data.ID_574087-1-develop (2)/SQLB2_Retrieving_data.ID_574087-1-develop directory

--- SQLB2_Retrieving_data.ID_574087-1-develop (2)/SQLB2_Retrieving_data.ID_574087-1-develop/src/ex05/day01_ex05.sql
+++
@@ -1,5 +0,0 @@
-SELECT person.id, person.name, person.age, person.gender, person.address, 
-       pizzeria.id AS pizzeria_id, pizzeria.name AS pizzeria_name, pizzeria.rating
-FROM person
-CROSS JOIN pizzeria
-ORDER BY person.id, pizzeria.id;
--- SQLB2_Retrieving_data.ID_574087-1-develop (2)/SQLB2_Retrieving_data.ID_574087-1-develop/src/ex07/day01_ex07.sql
+++
@@ -1,5 +0,0 @@
-SELECT order_date,
-       CONCAT(person.name, ' (age: ', person.age, ')') AS person_information
-FROM person_order
-NATURAL JOIN person
-ORDER BY order_date, person_information;
